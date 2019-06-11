fbf3d620c9e506b8666c8fc66c90adb8b42ab16a
Dolovan <dolovanfuz@gmail.com>
2019-06-11 14:32:10 +0200

    Update main.cs

--- sources/main.cs
+++ sources/main.cs
@@ -1,4 +1,3 @@
-﻿
 namespace Application
 {
     class Program
@@ -6,27 +5,30 @@ namespace Application
         static void Main(string[] args)
         {
             CApplicationDetectionObjets application = new CApplicationDetectionObjets();
-            
+
+            //Charger les fichiers XML
             application.ChargerEnvironnementsIdentifiables_depuisFichierXML(@"..\..\..\..\fichiers_XML\EnvironnementsIdentifiables.xml");
-
             application.ChargerImageOrientee_depuisFichierXML(@"..\..\..\..\fichiers_XML\ImageOrientee.xml");
-
             application.ChargerNuagePoint3D_depuisFichierXML(@"..\..\..\..\fichiers_XML\NuagePoints3D.xml");
-
             application.ChargerObjetsIdentifiables_depuisFichierXML(@"..\..\..\..\fichiers_XML\ObjetsIdentifiables.xml");
 
+
+            //Identifier les objets
             application.IdentifierObjets();
 
+            //Positionner les objet
             application.PositionnerObjetsDansImage();
 
+            //Génerer l'image de sortie
             application.GenererImageSortie(@"..\..\..\..\images\output.jpg");
 
+            //Génerer la scène 3D
             application.GenererScene3d();
             
+            //Exporter la scène 3D vers un fichier XML
             application.ExporterScene3DversFichierXML(@"..\..\..\..\fichiers_XML\sortie.xml");
 
-            application.AfficherRenduDetectionImage();
-            
+            //affichage            
             application.AfficherTout();
             
             while(true)
