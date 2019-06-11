ec63e88a00e64c78711c408193c587cb7ef87728
Dolovan <dolovanfuz@gmail.com>
2019-06-11 02:26:06 +0200

    Update main.cs

--- sources/main.cs
+++ sources/main.cs
@@ -8,44 +8,31 @@ namespace Application
             CApplicationDetectionObjets application = new CApplicationDetectionObjets();
             
             application.ChargerEnvironnementsIdentifiables_depuisFichierXML(@"..\..\..\..\fichiers_XML\EnvironnementsIdentifiables.xml");
-           //application.ChargerEnvironnementsIdentifiables_depuisXmlDocument(...);
 
             application.ChargerImageOrientee_depuisFichierXML(@"..\..\..\..\fichiers_XML\ImageOrientee.xml");
-            //application.ChargerImageOrientee_depuisXmlDocument(...);
 
             application.ChargerNuagePoint3D_depuisFichierXML(@"..\..\..\..\fichiers_XML\NuagePoints3D.xml");
-            ///application.ChargerNuagePoints3D_depuisXmlDocument(...);
 
             application.ChargerObjetsIdentifiables_depuisFichierXML(@"..\..\..\..\fichiers_XML\ObjetsIdentifiables.xml");
-            //application.ChargerEnvironnementsIdentifiables_depuisXmlDocument(...);
-
-           //application.parametrerDetection(0.1f);
-            /// Les paramètres sont: (float Min_score,string pbfile, string labelfile)
-            /// Min_score est le score minimu où l'on considère l'objet comme étant détecté
-            /// pbfile est le chemin vers le modèle (extension .pb)
-            /// labelfile est le chemin vers le fichier contenant les labelles du modèle (extensions .pbtxt)
 
             application.IdentifierObjets();
 
             application.PositionnerObjetsDansImage();
 
             application.GenererImageSortie(@"..\..\..\..\images\output.jpg");
-            //Le paramètre est optionnel, c'est le chemin du fichier de sortie.
 
-           //application.GenererScene3d();
+            application.GenererScene3d();
             
-            //application.ExporterScene3DversXMLDocument();
-           //application.ExporterScene3DversFichierXML(@"..\..\..\..\fichiers_XML\sortie.xml");
-
+            application.ExporterScene3DversFichierXML(@"..\..\..\..\fichiers_XML\sortie.xml");
 
             application.AfficherRenduDetectionImage();
-            //application.AfficherNuagePoints3D();
-            //application.AfficherScene3D();
-            //application.AfficherRenduDetectionConsole();
-            //application.AfficherObjetsIdentifiables();
-            //application.AfficherInfoImage();
-            //application.AfficherEnvironnemtsIdentifiables();
-           //application.AfficherTout();
+            
+            application.AfficherTout();
+            
+            while(true)
+            {
+                // boucle à enlever en production
+            }
         }
     }
 }
